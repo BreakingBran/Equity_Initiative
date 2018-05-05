980aa1a2ec6c53aa311a6eb6ed9a2e6e0abb0274
BreakingBran <lancepereira99@gmail.com>
2018-05-05 16:58:45 -0400

    Changed post.rb belongs_to user

--- Inspire/db/schema.rb
+++ Inspire/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_05_193408) do
+ActiveRecord::Schema.define(version: 2018_05_05_190535) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "message"
@@ -40,8 +40,6 @@ ActiveRecord::Schema.define(version: 2018_05_05_193408) do
     t.boolean "super_admin?"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
-    t.string "password"
   end
 
 end
