8be6ce454aad2cf2991c70de16d75a8f46f9af9f
James Lu <james.zhao.lu@gmail.com>
2018-05-05 15:36:24 -0400

    added resources and migrations

--- Inspire/db/schema.rb
+++ Inspire/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_05_190535) do
+ActiveRecord::Schema.define(version: 2018_05_05_193408) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "message"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2018_05_05_190535) do
     t.boolean "super_admin?"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "password"
   end
 
 end
