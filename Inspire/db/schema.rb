a9a5e56dbab7c8da1aa80db78c7b523823446f0e
James Lu <james.zhao.lu@gmail.com>
2018-05-05 16:15:47 -0400

    added last_name to user table

--- Inspire/db/schema.rb
+++ Inspire/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_05_193408) do
+ActiveRecord::Schema.define(version: 2018_05_05_201411) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "message"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2018_05_05_193408) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "password"
+    t.string "last_name"
   end
 
 end
