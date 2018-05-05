575f3f44de25b6d41c27d550629c526fce734cb1
James Lu <james.zhao.lu@gmail.com>
2018-05-05 18:06:13 -0400

    created user new

--- Inspire/app/controllers/users_controller.rb
+++ Inspire/app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		@user = User.new(user_params)
+
+		@user.save
+		redirect_to @user
 	end
 
 	def new
@@ -16,4 +20,9 @@ class UsersController < ApplicationController
 
 	def destroy
 	end
+
+	private
+  		def user_params
+    		params.require(:user).permit(:first_name, :last_name, :usename, :password, :bio)
+ 		 end
 end
