711ac4e019b4b4860a53cf8ac9b1a0f053c4bb89
James Lu <james.zhao.lu@gmail.com>
2018-05-05 19:14:04 -0400

    fixed user routing

--- Inspire/app/controllers/user_controller.rb
+++ Inspire/app/controllers/user_controller.rb
@@ -1,25 +1,19 @@
 class UserController < ApplicationController
 	def index
-
 	end
 
 	def create
-<<<<<<< HEAD:Inspire/app/controllers/users_controller.rb
 		@user = User.new(user_params)
 
 		@user.save
 		redirect_to @user
-=======
-
->>>>>>> 90c7ec54d83e3fb35ce52f6f08a3639a2eec4f1d:Inspire/app/controllers/user_controller.rb
 	end
 
 	def new
-
 	end
 
 	def show
-
+		@user = User.find(params[:id])
 	end
 
 	def update
--- Inspire/config/routes.rb
+++ Inspire/config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'welcome', to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users do
+  post 'user', to: 'user#create', as: 'user_create'
+
+  resources :user do
   	resources :posts
   	resources :comments do 
   		resources :replies
